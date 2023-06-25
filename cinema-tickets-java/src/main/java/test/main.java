a2d86856a5f04d914829ccbd8fd822621f6cbe8e
Kiran B L <57253045+kiranbl@users.noreply.github.com>
2023-06-25 13:39:15 +0100

    Second commit to the exercise

--- cinema-tickets-java/src/main/java/test/main.java
+++ cinema-tickets-java/src/main/java/test/main.java
@@ -1,11 +1,11 @@
-package main;
+package test;
 import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
-public class c {
+public class main{
     public static void main(String args[]){
 
         TicketServiceImpl t = new TicketServiceImpl();
-        TicketTypeRequest t1 = new TicketTypeRequest(TicketTypeRequest.Type.CHILD,010);
+        TicketTypeRequest t1 = new TicketTypeRequest(TicketTypeRequest.Type.CHILD,10);
         TicketTypeRequest t2 = new TicketTypeRequest(TicketTypeRequest.Type.ADULT,7);
         TicketTypeRequest t3 = new TicketTypeRequest(TicketTypeRequest.Type.INFANT,3);
         t.purchaseTickets(98765L,t1,t2,t3);
